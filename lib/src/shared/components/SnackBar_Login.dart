f1f7ea57ca43099b5ea1ae4874ee2a2b49ad2a92
rodrigo.rodrigues <rodrigorodrigues_0990@hotmail.com>
2023-09-19 20:14:16 -0300

    ajustes snackbar

--- lib/src/shared/components/SnackBar_Login.dart
+++ lib/src/shared/components/SnackBar_Login.dart
@@ -5,11 +5,27 @@ import 'package:flutter/material.dart';
 import '../colors/app_colors.dart';
 
 class AppSnackBar {
-
   static void showMassageUserNotFound(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.expense,
-      content: Text("Usuário não encontrado"),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.error_outline_rounded,
+              color: Color.fromARGB(255, 241, 223, 223),
+            ),
+          ),
+          Text(
+            "Usuário não encontrado",
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
@@ -18,7 +34,24 @@ class AppSnackBar {
   static void showMassageInvalidFormat(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.expense,
-      content: Text("Verifique os dados"),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.error_outline_rounded,
+              color: Color.fromARGB(255, 241, 223, 223),
+            ),
+          ),
+          Text(
+            "Verifique os dados",
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
@@ -27,7 +60,24 @@ class AppSnackBar {
   static void showMassageServerError(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.expense,
-      content: Text("Erro interno, tente mais tarde"),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.error_outline_rounded,
+              color: Color.fromARGB(255, 241, 223, 223),
+            ),
+          ),
+          Text(
+            "Erro interno, Tente mais tarde",
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
@@ -36,7 +86,24 @@ class AppSnackBar {
   static void showMassage(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.expense,
-      content: Text("Houve algum Erro"),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.error_outline_rounded,
+              color: Color.fromARGB(255, 241, 223, 223),
+            ),
+          ),
+          Text(
+            "Houve algum erro",
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
@@ -45,7 +112,24 @@ class AppSnackBar {
   static void showMassageUserAlreadyExists(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.expense,
-      content: Text("Esse usuário ja existe"),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.error_outline_rounded,
+              color: Color.fromARGB(255, 241, 223, 223),
+            ),
+          ),
+          Text(
+            "Esse usuário ja existe",
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
@@ -54,18 +138,24 @@ class AppSnackBar {
   static void showMassageValidate(context) {
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       backgroundColor: AppColors.logo,
-      content:
-      Row(
+      content: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(padding: EdgeInsets.only(right: 3),
-          child: Icon(Icons.check_circle_outline , color: Color.fromARGB(255, 54, 129, 57),),
+          Padding(
+            padding: EdgeInsets.only(right: 3),
+            child: Icon(
+              Icons.check_circle_outline,
+              color: Color.fromARGB(255, 54, 129, 57),
+            ),
           ),
-          Text("enviado"),
+          Text(
+            "Enviado",
+            style: TextStyle(fontSize: 15),
+          ),
         ],
-      ) ,
+      ),
       behavior: SnackBarBehavior.floating,
       duration: Duration(seconds: 1),
     ));
